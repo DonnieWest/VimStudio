d94de84adbddee4dbee0a1e9d17ac7b2a8c0f59c
Donnie West <donnie@codekoalas.com>
2015-11-24 12:53:37 -0600

    Add neomake support

--- rplugin/python3/VimStudio/Configurator.py
+++ rplugin/python3/VimStudio/Configurator.py
@@ -11,3 +11,4 @@ class Configurator(object):
         classpath.extend(PathsFinder(self.vim).getAllSourcePaths())
         sourcepath = ':'.join(classpath)
         self.vim.command("let g:syntastic_java_javac_classpath = '" + sourcepath + "'")
+        self.vim.comamnd("let g:neomake_java_javac_args = neomake#makers#ft#java#javac()['args'] + ['-cp', '" + sourcepath + "']")
