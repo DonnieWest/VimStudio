0e070b756f70b6a330e0d2701f0413c8a4c2c8d8
Donnie West <me@donniewest.com>
2016-12-03 18:09:32 -0600

    Add commands for launching the SDK manager and the EmulatorManager

--- rplugin/python3/VimStudio/__init__.py
+++ rplugin/python3/VimStudio/__init__.py
@@ -86,6 +86,14 @@ class VimStudio(object):
     def launchEmulator(self, args):
         self.Configurator.launchEmulator(args[0])
 
+    @neovim.command("VimStudioEmulatorManager")
+    def launchEmulatorManager(self):
+        self.Configurator.launchEmulatorManager()
+
+    @neovim.command("VimStudioSDKManager")
+    def launchEmulatorManager(self):
+        self.Configurator.launchSDKManager()
+
     @neovim.autocmd("BufReadPost", pattern="*.java")
     def setup(self):
         self.setupVimStudio()
